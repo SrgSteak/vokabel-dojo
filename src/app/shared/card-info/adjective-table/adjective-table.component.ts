87a29f3c6cce7f7e1f7e5b5902e4b2087553b633
Robin Schmidt <schmidt91robin@icloud.com>
2022-08-01 23:53:40 +0200

    Dev (#52)
    * github actions (bugfix): dropping node v12 since it is deprecated
    
    * Update node.js.yml
    
    * dependencies (bugfix): fixed mix up that blocked karma
    
    * angular-feather (bugfix): forced v6 although it "needs" ang14
    
    * github actions (debugging): deactivated node 16 for now
    
    * angular-feather (bugfix): limited to last compatible ang12 version
    
    * about (bugfix): fixed broken link to nihongo gakuen
    
    * setup (enhancement): fixed command, added troubleshoot to readme
    
    * about (bugfix): added missing chevron left
    
    * menu (enhancement): added missing top navigation to jouyou kanji
    
    * card info adjectives (enhancement): covering い and な adjective flexions now
    
    * card info verbs (enhancement): covering ます形 and 普通形 now
    
    still adding て形
    
    * dependabot (enhancement): should trigger PR requests to dev branch and not master anymore

--- src/app/shared/card-info/adjective-table/adjective-table.component.ts
+++ src/app/shared/card-info/adjective-table/adjective-table.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Card } from 'src/app/core/entities/card';
+import { CardInterface } from 'src/app/core/entities/card-interface';
 import { AdjectiveType } from 'src/app/core/entities/card-type';
 
 export enum konjugationType {
   default = 0,
-  negated = 1
+  negated = 1,
+  past = 2,
+  negatedPast = 3
 }
 
 @Component({
   selector: 'app-adjective-table',
   templateUrl: './adjective-table.component.html',
-  styleUrls: ['./adjective-table.component.css']
+  styleUrls: ['./adjective-table.component.scss']
 })
 export class AdjectiveTableComponent implements OnInit {
 
-  @Input() card: Card;
+  @Input() card: CardInterface;
 
   get konjugationTypes() {
     return konjugationType;
@@ -25,22 +27,32 @@ export class AdjectiveTableComponent implements OnInit {
   ngOnInit() {
   }
 
-  flexLemma(card: Card, destination: konjugationType): string {
+  flexLemma(card: CardInterface, destination: konjugationType): string {
     switch (card.adjectiveType) {
+      // い形容詞
       case AdjectiveType.iAdjective:
         switch (destination) {
           case konjugationType.default:
             return card.japanese;
+          case konjugationType.past:
+            return card.japanese.substring(0, card.japanese.length - 1) + 'かった';
           case konjugationType.negated:
             return card.japanese.substring(0, card.japanese.length - 1) + 'くない';
+          case konjugationType.negatedPast:
+            return card.japanese.substring(0, card.japanese.length - 1) + 'くなかった';
         }
 
+      // な形容詞
       case AdjectiveType.naAdjective:
         switch (destination) {
           case konjugationType.default:
             return card.japanese;
+          case konjugationType.past:
+            return card.japanese + 'でした'
           case konjugationType.negated:
             return card.japanese + 'ではありません';
+          case konjugationType.negatedPast:
+            return card.japanese + 'ではありませんでした';
         }
     }
   }
